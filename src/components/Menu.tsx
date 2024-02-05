ec3db9829274ee58cc0f95314555d9c7c1e0d417
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-02-05 17:47:58 +0100

    add visual indicator menu opened or closed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
+import { VscClose } from "react-icons/vsc";
 
 export default function Menu() {
 
@@ -10,7 +11,8 @@ export default function Menu() {
     return (
         <header className="bg-gray text-white font-bold">
             <nav className='flex justify-between p-6 md:text-xl'>
-                <FiMenu className='cursor text-xl z-10 md:hidden sm:block' onClick={() => setOpen(!open)} />
+                <VscClose className={`cursor text-xl z-10 md:hidden ${open ? 'sm:block' : 'hidden'}`} onClick={() => setOpen(!open)} />
+                <FiMenu className={`cursor text-xl z-10 md:hidden ${!open ? 'sm:block' : 'hidden'}`} onClick={() => setOpen(!open)} />
                 <ul className={`${open ? 'block bg-gray w-screen px-7' : 'hidden'} md:flex absolute left-0 py-7 flex-col md:static md:py-0 md:w-min md:transform-none md:border-none md:flex-row`}>
                     <li className='py-2 md:py-0 md:mr-6'>
                         <a href="/" className='link-underline'>Acceuil</a>
