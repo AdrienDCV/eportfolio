0c16d9b310f5689fa501e1b118cdf1c14984ff08
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-02-05 18:56:06 +0100

    component optimization

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,33 +8,29 @@ export default function Menu() {
 
     const [open, setOpen] = useState(true)
 
+    const links = [
+        { id: 1, pageName: 'Acceuil', url: '/home' },
+        { id: 2, pageName: 'Présentation', url: '/presentation' },
+        { id: 3, pageName: 'Fromation', url: '/formation' },
+        { id: 4, pageName: 'Carrière', url: '/career' },
+        { id: 5, pageName: 'Compétences', url: '/skils' },
+        { id: 6, pageName: 'Réalisations', url: '/projects' },
+        { id: 7, pageName: 'Contact', url: '/contact' }
+    ]
+
     return (
         <header className="bg-gray text-white font-bold">
             <nav className='flex justify-between p-6 md:text-xl'>
                 <VscClose className={`cursor text-xl z-10 md:hidden ${open ? 'sm:block' : 'hidden'}`} onClick={() => setOpen(!open)} />
                 <FiMenu className={`cursor text-xl z-10 md:hidden ${!open ? 'sm:block' : 'hidden'}`} onClick={() => setOpen(!open)} />
                 <ul className={`${open ? 'block bg-gray w-screen px-7' : 'hidden'} md:flex absolute left-0 py-7 flex-col md:static md:py-0 md:w-min md:transform-none md:border-none md:flex-row`}>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/" className='link-underline'>Acceuil</a>
-                    </li>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/ "className='link-underline'>Présentation</a>
-                    </li>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/" className='link-underline'>Formation</a>
-                    </li>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/" className='link-underline'>Carrière</a>
-                    </li>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/" className='link-underline'>Compétences</a>
-                    </li>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/" className='link-underline'>Réalisations</a>
-                    </li>
-                    <li className='py-2 md:py-0 md:mr-6'>
-                        <a href="/" className='link-underline'>Contact</a>
-                    </li>
+                    { 
+                        links.map( (link) => (
+                            <li  key={ link.id } className='py-2 md:py-0 md:mr-6'>
+                                <a href={ link.url } className='link-underline'>{ link.pageName }</a>
+                            </li>
+                        )) 
+                    }
                 </ul>
             </nav>
         </header>
