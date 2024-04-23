148a4a3f0a144c537f0ce07c5a1b6b1946e4e669
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-04-23 21:17:08 +0200

    remove dist folder

--- src/app/presentation/page.tsx
+++ src/app/presentation/page.tsx
@@ -1,7 +1,121 @@
 export default function Presentation () {
 
     return (
-        <h1>Presentation</h1>
+        <>
+            <h1 className="text-gray font-bold text-4xl">Présentation</h1>
+            
+            <div>
+                <h2 className="text-gray font-bold text-2xl">Qui-suis je ?</h2>
+                <p className="whitespace-pre-line">
+                    { 
+                        `
+                            Je m'appelle Adrien DA COSTA VEIGA, j'ai 20 ans et je suis originaire de l'Oise (60), en Picardie.
+
+                            Je suis actuellement en troisème année de Bachelor Universitaire de Technologie (BUT) Informatique, parcours Réalisation d'applications : conception, développement et validation
+                            à l'Institut Universitaire de Technologie de Lille, site de Villeneuve d'Ascq. 
+                            Je réalise cette dernière année de BUT Informatique en apprentissage au sein de l'entreprise Toyota Motor Manufacturing France à Onnaing, au poste 
+                            d'apprenti développeur full stack où je participe au développement, déploiement et à la maintenance d'applications visant à soutenir les opérateurs et opératrices sur la ligne de production.
+
+                            Je suis passionné par le monde de l'informatique depuis mes 12 ans. C'est à cet âge que j'ai obtenu mon tout premier ordinateur personnel. Au moment de la sélection des composants
+                            et du montage de cet ordinateur. Je me posais beaucoup de questions concernant le fonctionnement des composants. Comment est-ce que la carte mère fonctionne ? Comment est-ce que les
+                            informations sont-elles stockées dans le disque dur ? Comment est-ce que le système sait à quel endroit aller chercher telle ou telle information ?
+                            Telles étaient les questions qui m'ont poussé à m'intéresser à ce vaster domaine qu'est l'informatique.
+
+                            C'était décidé. Je souhaitais réaliser ma carrière professionnelle dans ce domaine. Je ferai de cette passion mon métier !
+                            Depuis lors, j'ambitionne de devenir Ingénieur Informatique.
+
+                            C'est pour cela que je suis très fier d'avoir la possibilité de poursuivre mes études au sein de 
+                            l'Institut National des Sciences Appliquées (INSA) Hauts-de-France en cursus Ingénieur spécialité Informatique en apprentissage pour une durée totale de 3 ans, soit 36 mois.
+
+                            On dit que "la curiosité est un vilain défaut". Cela est peut-être vrai, mais pour moi, il s'agit d'une grande qualité !
+                            De par ma curiosité et ma soiffe de savoir, j'en viens à m'intéresser à tout type de sujet. Sciences, technologies, littérature, culture et j'en passe.
+                            A cette curiosité s'ajoute une grande rigueur, appréciant que le travail soit fait et ce correctement. Une rigueur que je m'éforce d'appliquée également dans 
+                            ma façon de programmer, cherchant à toujours convenir au mieux aux conventions ainsi qu'à toujours chercher à optimiser au mieux l'écriture du code que je produis.  
+                            Je suis quelqu'un de persévérant, qui n'a pas peur de se remttre en question et qui cherche à s'améliorer en permanance.
+
+                            En dehors de l'informatique, j'éprouve un fort intérêt pour de nombreux autres domaines biens différents.
+
+                            Voici mes passions les plus importantes :
+                        `
+                    }
+                </p>
+            </div>
+            <div>
+                <div>
+                    <h3>Sport Automobile</h3>
+                    <div>
+                        <img src="" alt=""/>
+                        <p className="whitespace-pre-line">
+                            {
+                                `
+                                    Depuis maintenant quelques années, je suis épris d'une forte passion pour le monde du sport automobile.
+                                    Cela a commencé en écoutant un ami qui parlait de Formule 1. Pris d'un élan de curiosité, je suis allé regarder
+                                    quelques vidéos sur ce sport qui, bien qu'en ayant déjà entendu parlé et regardé lorsque j'étais plus petit, ne
+                                    m'avait pourtant jamais vraiment intéressé. Je regarde donc une vidéo, deux vidéos, trois vidéos et je me retrouve
+                                    finalement à rapidement avoir parcouru l'histoire de la catégorie reine du sport automobile. De Fangio à Verstappen,
+                                    je suis tombé amoureux de ce domaine.
+
+                                    J'ai donc commencé à suivre assiduement la Formule 1, puis je me suis rapidement ouvert à d'autres disciplines
+                                    comme l'endurance (World Endurance Championship), la Formule 2 ou même le Rallye (World Rally Championship) où je supporte
+                                    respectivement Alpine, Art Grand Prix et Toyota GAZOO Racing en WEC et WRC.
+                                    Au-delà des moteurs qui font du bruit, l'aérodynamisme, l'ingénierie mécanique et
+                                    le contexte de recherche permetante d'innovation qui me passionne et fascine dans ce domaine. 
+                                `
+                            }
+                        </p>
+                    </div>
+                </div>
+
+                <div>
+                    <h3>Histoire et culture du monde</h3>
+                    <div>
+                        <img src="" alt=""/>
+                        <p className="whitespace-pre-line">
+                            {
+                                `
+                                    L'Histoire, de la France et du monde, a toujours joué un rôle prépondérant dans ma vie. 
+                                    Depuis ma plus tendre enfance, jusqu'à aujourd'hui et pour bien des années encore. 
+                                    Plus le temps avance, plus nous en apprenons sur celui qui est passé. Savoir et comprendre 
+                                    le mode de vie de nos prédécesseurs m'a toujours fasciné, je ne peux m'empêcher d'imaginer l'ambiance 
+                                    qui régnait à telle ou telle époque, les difficultés par lesquelles les Poilus de 14-18 sont passés, 
+                                    la surprise générale de l'ancien monde lorsque Christophe Colomb fit connaître l'Amérique 
+                                    (et non découvrir, les Vikings ont déjà foulé le sol américain bien avant les Conquistadors), 
+                                    la constructions des pyramides, les premières civilisations humaines, les premiers pas de l'homme 
+                                    sur deux jambes et d'inombrables autres événements qui ont et continuent d'enrichir un des plus grands 
+                                    joyaux de l'humanité. Connaître son passé, ses merveilles mais également et peut-être surtout ses fautes 
+                                    permettent de mieux envisager son futur. Connaître l'Histoire, c'est aussi savoir que des erreurs ont 
+                                    été commises par le passé. Il est primordial de reconnaître l'existence de ces erreurs, afin d'en tirer des 
+                                    leçons pour qu'elles ne puissent se reproduire.
+                                `
+                            }
+                        </p>
+                    </div>
+                </div>
+
+                <div>
+                    <h3>Aéronautique et aérospatial</h3>
+                    <div>
+                        <img src="" alt=""/>
+                        <p className="whitespace-pre-line">
+                            {
+                                `
+                                    Comme beaucoup de mes centres d'intérêts, c'est dans mon enfance (et presque depuis ma naissance) que je suis épris 
+                                    d'un fort intérêt pour l'aéronautique et l'aérospatial. Il s'agit d'une passion que je partage avec mon père 
+                                    (on pourrait dire que j'en ai certainement hérité), lui aussi grand amateur d'aviation. Je me souviens encore 
+                                    du petit Adrien en maternelle clamer haut et fort qu'il allait "travailler dans les tours de contrôle" 
+                                    (à l'époque je ne savais pas que le nom exact était "Aiguilleur du ciel"). En ce qui concerne l'aérospatial, 
+                                    j'ai toujours été intrigué par l'immensité et les mysètres que représente l'espace et l'univers dans 
+                                    lequel notre toute petite planète flotte élégamment. Encore aujourd'hui, je ne saurais expliquer comment est né 
+                                    cet intérêt pour ces oiseaux de fer et ces missiles habitables. 
+                                    Sûrement cette incontrolable envie pour l'homme de voler et se ressentir la liberté qu'est celle offerte aux 
+                                    volatiles qui peuplent le ciel.
+                                `
+                            }
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 
 }
