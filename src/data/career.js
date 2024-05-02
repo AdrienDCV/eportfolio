6dd8f3d37ab6f19ecdc0ab9bf5e86a3e9b090385
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-05-02 20:05:51 +0200

    FEAT: add Career page components

---
+++ src/data/career.js
@@ -0,0 +1,23 @@
+const career = [
+    {
+        title: 'Développeur full stack Java / JS',
+        type: 'Alternance',
+        society: 'Toyota Motor Manufacturing France',
+        date: 'septembre 2023 - août 2024',
+        duration: '1 an',
+        place: 'Onnaing, Nord (59), France',
+        descirption: 'Développement, déploiement et maintenance d\'applications web en support à la production',
+        logo: 'https://d1guu6n8gz71j.cloudfront.net/system/asset/logos/1052284/logo.png?1510043350'
+    },
+    {
+        title: 'Développeur full stack Java / JS',
+        type: 'Stage',
+        society: 'Toyota Motor Manufacturing France',
+        date: 'septembre 2023 - août 2024',
+        duration: '1 an',
+        place: 'Onnaing, Nord (59), France',
+        descirption: 'Développement, déploiement et maintenance d\'applications web en support à la production',
+        logo: 'https://d1guu6n8gz71j.cloudfront.net/system/asset/logos/1052284/logo.png?1510043350'
+    }
+]
+export default career
