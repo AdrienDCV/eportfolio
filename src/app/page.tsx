d9e095b31a9502bd7b472f4bc1d980cf22b123ea
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-05-21 21:16:30 +0200

    BUILD: new version of the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Menu from "@/components/Menu";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+export default function page () {
+   
+    return (
+        <div className="flex flex-col justify-center items-center mt-10">
+            <h1 className="text-gray font-bold text-4xl">Bienvenue !</h1>
+            <div className="flex flex-col md:flex-row md:p-10 md:mt-10 justify-center items-center">
+                <Image src={'/images/photo_CV.png'} alt={"photo_CV_DACOSTAVEIGA_Adrien"} width={200} height={200} className="mt-5"/>
+                <div className="md:block hidden border-l-4 border-gray mt-5 w-4 h-[250px] ml-20"></div>
+                <div className="block md:hidden border-t-4 border-gray mt-5 w-[85%]"></div>
+                <p className="p-6 md:ml-20 md:w-[750px]">
+                    Enchanté,
+                    <br />  
+                    Je me nomme <span className='font-bold'>Adrien DACOSTA VEIGA</span>, 20 ans.<br />
+                    <br />
+                    <span className="font-bold">Passionné</span> depuis maintenant quelques années par <span className="font-bold">le monde de l’informatique</span>,
+                    j’ai décidé de <span className="font-bold">faire de cette passion mon métier</span>.<br />
+                    <br />
+                    C’est dans cette optique que j’ai entrepris un <span className="font-bold">Bachelor Universitaire de Technologie (BUT) en Informatique</span> à
+                    l’Institut Universitaire de Technologie de Lille site de Villeneuve d’Ascq.<br />
+                    Actuellement en <span className="font-bold">troisième année parcours réalisation d’applications : conception, développement,
+                    validation</span> que je réalise en <span className="font-bold">alternance</span> au sein de l’entreprise
+                    <span className="font-bold">Toyota Motor Manufacturing France</span>, au poste <span className="font-bold">d’apprenti développeur full stack</span>.<br />
+                    <br />
 
-export const metadata: Metadata = {
-  title: "ePortfolio - Adrien DA COSTA VEIGA",
-  description: "My ePortfolio",
-};
+                    Sur ce site, vous trouverez une <a href="/presentation"><span className="font-bold">présentation</span></a> plus détaillée afin de mieux apprendre à
+                    me connaître ainsi que de mon <a href="/formation"><span className="font-bold">parcours scolaire et universitaire</span></a>, 
+                    les <a href="/projects"><span className="font-bold">projets réalisés</span></a> dans le cadre de mes études et/ou personnels et 
+                    mon <a href="/formation"><span className="font-bold">parcours professionnel</span></a>.<br />
+                    <br />
+                    Je vous remercie pour l’attention accordée et le temps dédié à la lecture de ces articles !
+                </p>
+            </div>
+        </div>
+    )
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-  
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Menu />
-        {children}
-      </body>
-    </html>
-  );
-  
 }
