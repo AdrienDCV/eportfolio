375ebc019abcb348a48ac82f7657e4a02b043509
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-05-25 14:39:07 +0200

    FIX: fix Subtitle component imports

--- src/components/title/SubSubTitle.tsx
+++ src/components/title/SubSubTitle.tsx
@@ -1,3 +1,3 @@
-export default function SubTitle({ text }: { text: string }) {
+export default function Subtitle({ text }: { text: string }) {
     return <h3 className="text-gray font-bold text-xl">{ text }</h3>
 }
